3d242fdd0ebf359686edde6305de34a74e635a62
Daniel Alvero <dalvero589@gmail.com>
2025-11-02 23:44:37 +0700

    fix bugsssssss

--- lib/services/studentService.ts
+++ lib/services/studentService.ts
@@ -1,5 +1,5 @@
 import { getSupabaseClient } from "../supabaseClient";
-import { Student } from "../types/students";
+import { Students } from "../types/students";
 
 /**
  * studentService
@@ -12,9 +12,9 @@ export const studentService = {
    * ----------------------------
    * Mengambil semua data siswa dari tabel `students`, termasuk detail sekolah.
    * 
-   * @returns {Promise<Student[]>} - Daftar semua siswa.
+   * @returns {Promise<Students[]>} - Daftar semua siswa.   
    */
-  async getAll(): Promise<Student[]> {
+  async getAll(): Promise<Students[]> {
     const supabase = getSupabaseClient();
     if (!supabase) throw new Error("Supabase client not initialized.");
     const { data, error } = await supabase
@@ -33,30 +33,11 @@ export const studentService = {
    * 
    * @param student - Data siswa yang akan ditambahkan, tidak termasuk `id` dan `created_at`.
    */
-  async add(student: Omit<Student, "created_at">): Promise<void> {
+  async add(student: Omit<Students, "created_at">): Promise<void> {
     const supabase = getSupabaseClient();
     if (!supabase) throw new Error("Supabase client not initialized.");
     const { error } = await supabase.from("students").insert([student]);
     if (error) throw error;
-
-    // ADD KE TABEL USERS AGAR TETAP SINKRON
-    const { error: usersError } = await supabase.from("users").insert([
-      {
-        id: student.id,
-        username: `${student.nama_depan}_${student.nama_belakang}`.toLowerCase(),
-        email: "student@gmail.com",
-        address: student.address || null,
-        city: student.city || null,
-        role: "student",
-        created_at: new Date().toISOString(),
-      },
-    ]);
-
-    if (usersError)
-      throw new Error(
-        `Gagal menyimpan data ke tabel users: ${usersError.message}`        
-      );
-      console.log(`Gagal menyimpan data ke tabel users: error`);
   },
 
   /**
@@ -81,7 +62,7 @@ export const studentService = {
    * @param id - UUID siswa yang akan diperbarui.
    * @param updated - Data siswa yang akan diperbarui, tidak termasuk `id` dan `created_at`.
    */
-  async update(id: string, updated: Partial<Student>): Promise<void> {
+  async update(id: string, updated: Partial<Students>): Promise<void> {
       const supabase = getSupabaseClient();
       if (!supabase) throw new Error("Supabase client not initialized.");
       const { error } = await supabase.from("students").update(updated).eq("id", id);
